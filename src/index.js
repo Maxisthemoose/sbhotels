2a6fe4fdf01cb078279ad45b4c7a22360268efa4
Maxisthemoose <oriontothemax@gmail.com>
2023-03-12 11:35:18 -0700

    hash router for gh-pages (temp)

--- src/index.js
+++ src/index.js
@@ -6,17 +6,16 @@ import './index.css';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import App from './App';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-// import Nav from './components/Nav/Nav';
+import { HashRouter, Route, RouterProvider, Routes, createBrowserRouter, createHashRouter } from 'react-router-dom';
+import Nav from './components/Nav/Nav';
 import DirectionsMap from './routes/DirectionsMap';
 import Root from './routes/Root';
 import LandingPage from './routes/LandingPage';
 import Brisas from './routes/Brisas';
 import Harbor from './routes/Harbor';
 import Lavender from './routes/Lavender';
-const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const router = createBrowserRouter(
+const router = createHashRouter(
   [
     {
       path: "/",
@@ -46,10 +45,23 @@ const router = createBrowserRouter(
     },
 
   ]
-)
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   // <React.StrictMode>
-  <RouterProvider router={router} />
+  <HashRouter basename='/'>
+    <Nav />
+    <Routes>
+      <Route exact path="/" element={<LandingPage />} />
+      <Route path="/maps" element={<DirectionsMap />} />
+      <Route path="/brisas" element={<Brisas />} />
+      <Route path="/harbor" element={<Harbor />} />
+      <Route path="/lavender" element={<Lavender />} />
+    </Routes>
+    {/* <RouterProvider router={router} /> */}
+  </HashRouter>
+  // <RouterProvider router={router} />
   // </React.StrictMode>
 );
